b44af5db84912e7f93f4df126e3c4a3708bd6527
kam <kammif1990@gmail.com>
2017-03-30 17:05:08 +0500

    add_templane_and_entry

--- djangos_ll/models.py
+++ djangos_ll/models.py
@@ -6,3 +6,16 @@ class Topic(models.Model):
     def __str__(self):
         """Return string views model"""
         return self.text
+
+class Entry(models.Model):
+    """Info lernings user on them"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            """Return str views model"""
+            return self.text[:50] + "..."
